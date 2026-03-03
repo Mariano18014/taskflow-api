a49113dba344b200db2a448e5be14bb22fbfca32
Mariano18014 <hasanmariano5@gmail.com>
2026-03-03 04:51:38 -0300

    feat: implementación de manejo global de errores.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
+import notFound from "./middlewares/notFound.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
